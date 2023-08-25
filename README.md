b90d6c2724769e6f60e3f0a4a3739aef6eef6c73
Enrique <enrique.sousa@gmail.com>
2023-08-25 08:58:16 -0700

    Commit phpinfo

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # phpinfo
+Para Instalar phpMyAdmin (https://www.phpmyadmin.net/)
+Bajarlo y Guardarlo en una carpeta por ejemplo ~/Sites/phpMyAdmin
+Lo podemos correr abriendo index.php
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    /* 
+    Para Instalar phpMyAdmin (https://www.phpmyadmin.net/)
+    Bajarlo y Guardarlo en una carpeta por ejemplo ~/Sites/phpMyAdmin
+    Lo podemos correr abriendo index.php
+    */
+    echo phpinfo();
+?>
